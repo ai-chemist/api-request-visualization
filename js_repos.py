e489ed6dbe32547cddf6912cbbf5004b8e1dc108
ai-chemist <utk8244@gmail.com>
2025-08-27 19:17:39 +0900

    Repositories list checked

--- js_repos.py
+++ js_repos.py
@@ -16,11 +16,17 @@ print(f"Complete results : {not response_dict['incomplete_results']}")
 repo_dicts = response_dict['items']
 print(f"Repositories returned : {len(repo_dicts)}")
 
-# 첫 번째 저장소 분석
-repo_dict = repo_dicts[0]
-print(f"\nKeys : {len(repo_dict)}")
-for key in repo_dicts[0]:
-    print(key)
+# 받아온 저장소 분석
+print("Selected information about each Repository:")
+for repo_dict in repo_dicts:
+    print(f"\n Selected information about first repository: ")
+    print(f"Name : {repo_dict['name']}")
+    print(f"Owner : {repo_dict['owner']['login']}")
+    print(f"Stars : {repo_dict['stargazers_count']}")
+    print(f"Repository : {repo_dict['html_url']}")
+    print(f"Created : {repo_dict['created_at']}")
+    print(f"Updated : {repo_dict['updated_at']}")
+    print(f"Description : {repo_dict['description']}")
 
 # 결과 처리
 print(response_dict.keys())
